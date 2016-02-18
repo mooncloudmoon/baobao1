3802d670f55e6d42ac8b73d6cb822a6b360b511e
unknown <jbshang@LIU-PC.landhightech.com>
2016-02-18 16:26:36 +0800

    db config 0

---
+++ includes/core.inc.php
@@ -0,0 +1,10 @@
+<?php
+
+$db_config_0 = array(
+    type          => mysql,
+    host          => localhost,
+    user          => root,
+    password      => root,
+    database      => test,
+    prefix        => test_
+);
