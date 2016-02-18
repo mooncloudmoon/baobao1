e9707425b4c35ce4797c8469de309c16a924fad2
unknown <jbshang@LIU-PC.landhightech.com>
2016-02-18 16:46:49 +0800

    SQL.class.php  20160218-0

--- classes/SQL.class.php
+++ classes/SQL.class.php
@@ -2,7 +2,20 @@
 require_once(dirname(__FILE__).'/../includes/core.inc.php');
 
 class SQL{
+    private static $instance = null;
 
+    private $link = false;
+    private $db = null;
+    private $result = false;
 
+    public $sqltype;
+    public $sqlhost;
+    public $sqluser;
+    public $sqlpass;
+    public $sqlbase;
+    public $prefix;
 
+    private $total_queries = 0;
+
+    
 }
